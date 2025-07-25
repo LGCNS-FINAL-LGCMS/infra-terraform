4ae275aefa1a0fd3fb814fad7b73b9cf9226a691
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-07-25 14:55:22 +0900

    [LB-164] feat : argocd & backend 네임스페이스 분리

--- local/argo.tf
+++ local/argo.tf
@@ -1,34 +1,53 @@
-# resource "kubernetes_namespace" "argo_ns" {
-#   metadata {
-#     annotations = {
-#       name = "argocd"
-#     }
-#
-#     name = "argocd"
-#   }
-# }
+resource "kubernetes_namespace" "argo_ns" {
+  metadata {
+    annotations = {
+      name = "argocd"
+    }
+
+    labels = {
+      "argocd.argoproj.io/managed-by" = "argocd"
+    }
+
+    name = "argocd"
+  }
+}
+
+resource "kubernetes_namespace" "backend" {
+  metadata {
+    annotations = {
+      name = "backend"
+    }
+
+    labels = {
+      "argocd.argoproj.io/managed-by" = "argocd"
+    }
+
+    name = "backend"
+  }
+}
 
 resource "helm_release" "argo-cd" {
   name = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
   chart = "argo-cd"
   version = "8.1.3"
-  namespace = "default"
+  namespace = "argocd"
 
   values = [
     file("../values/local/argocd/argocd_values.yaml")
   ]
 
-#   depends_on = [
-#     kubernetes_namespace.argo_ns
-#   ]
+  depends_on = [
+    kubernetes_namespace.argo_ns
+  ]
 }
 
 resource "kubernetes_manifest" "argocd-github-access" {
   manifest = yamldecode(file("../values/local/argocd/argocd_github_secret.yaml"))
-#   depends_on = [
-#     kubernetes_namespace.argo_ns
-#   ]
+  depends_on = [
+    kubernetes_namespace.argo_ns,
+    kubernetes_namespace.backend
+  ]
 }
 
 resource "helm_release" "argocd-apps" {
@@ -36,7 +55,7 @@ resource "helm_release" "argocd-apps" {
   repository = "https://argoproj.github.io/argo-helm"
   chart = "argocd-apps"
   version = "2.0.2"
-  namespace = "default"
+  namespace = "argocd"
 
   values = [
     file("../values/local/argocd/argocd_apps_values.yaml")
@@ -44,6 +63,6 @@ resource "helm_release" "argocd-apps" {
 
   depends_on = [
     helm_release.argo-cd,
-    kubernetes_manifest.argocd-github-access
+    kubernetes_manifest.argocd-github-access,
   ]
 }
