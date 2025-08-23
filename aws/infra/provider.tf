e5843ffbd4c2f8a6e911a39d23d418b8fda71f66
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-08-23 10:02:12 +0900

    [LB-303] feat : RDB 데이터베이스 초기화 스크립트 블록 추가

--- aws/infra/provider.tf
+++ aws/infra/provider.tf
@@ -1,13 +1,26 @@
 terraform {
+  backend "s3" {
+    bucket       = "lgcms-terraform-state-bucket"
+    key          = "infra/terraform.tfstate"
+    region       = "ap-northeast-2"
+    profile      = "lgcms-dev"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.8.0"
+      version = "6.9.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
     }
   }
 }
 
 provider "aws" {
   profile = var.aws_profile
-  region = "ap-northeast-2"
+  region  = "ap-northeast-2"
 }
--- aws/infra/rds.tf
+++ aws/infra/rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "main" {
-  name = "${var.environment}-db-subnet-group"
+  name       = "${var.environment}-db-subnet-group"
   subnet_ids = aws_subnet.private[*].id
 
   tags = {
@@ -10,16 +10,16 @@ resource "aws_db_subnet_group" "main" {
 resource "aws_db_instance" "main" {
   identifier = "${var.environment}-postgres-db"
 
-  engine = "postgres"
+  engine         = "postgres"
   engine_version = "17.5"
   instance_class = var.rds_instance_class
 
-  allocated_storage = var.rds_allocated_storage
+  allocated_storage     = var.rds_allocated_storage
   max_allocated_storage = var.rds_allocated_storage
-  storage_type = "gp3"
-  storage_encrypted = true
+  storage_type          = "gp3"
+  storage_encrypted     = true
 
-  db_name = var.db_name
+  db_name  = var.db_name
   username = var.db_username
   password = var.db_password
 
@@ -35,5 +35,37 @@ resource "aws_db_instance" "main" {
 
   tags = {
     Name = "${var.environment}-postgres-db"
+  }
+}
+
+resource "null_resource" "init_rds_schema" {
+  depends_on = [aws_instance.bastion, aws_db_instance.main]
+
+  provisioner "file" {
+    source      = "init-sql/init.sql"
+    destination = "/tmp/init.sql"
+
+    connection {
+      type = "ssh"
+      host = aws_eip.bastion.public_ip
+      user = "ubuntu"
+      private_key = file(var.bastion_keypair_path)
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y postgresql-client",
+      # "export PGPASSWORD='${var.db_password}'",
+      "PGPASSWORD=${var.db_password} psql -h ${aws_db_instance.main.address} -p ${aws_db_instance.main.port} -U ${var.db_username} -f /tmp/init.sql"
+    ]
+
+    connection {
+      type = "ssh"
+      host = aws_eip.bastion.public_ip
+      user = "ubuntu"
+      private_key = file(var.bastion_keypair_path)
+    }
   }
 }
