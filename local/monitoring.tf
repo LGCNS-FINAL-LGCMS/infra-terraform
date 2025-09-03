e889bc693da5d418d545eade914f3c4e461647b5
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-09-03 15:18:42 +0900

    [LB-342] feat : promtail 배포 설정

--- local/monitoring.tf
+++ local/monitoring.tf
@@ -130,6 +130,67 @@ resource "helm_release" "loki" {
   ]
 }
 
+resource "helm_release" "promtail" {
+  name       = "promtail"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argocd-apps"
+  version    = "2.0.2"
+  namespace  = "argocd"
+
+  atomic            = true
+  cleanup_on_fail   = true
+  dependency_update = true
+
+  values = [
+    yamlencode({
+      applications = {
+        promtail = {
+          namespace = "argocd"
+          project   = "default"
+          sources = [
+            {
+              chart          = "promtail"
+              repoURL        = "https://grafana.github.io/helm-charts"
+              targetRevision = "6.17.0"
+              helm = {
+                valueFiles = [
+                  "$values/local/monitoring/promtail-values.yaml"
+                ]
+              }
+            },
+            {
+              repoURL        = "https://github.com/LGCNS-FINAL-LGCMS/infra-helm-values.git"
+              targetRevision = "main"
+              ref            = "values"
+            }
+          ]
+          destination = {
+            server    = "https://kubernetes.default.svc"
+            namespace = "monitoring"
+          }
+          syncPolicy = {
+            automated = {
+              prune    = true
+              selfHeal = true
+            }
+            syncOptions = [
+              "CreateNamespace=true",
+              "ServerSideApply=true",
+              "argocd.argoproj.io/sync-wave=0"
+            ]
+          }
+        }
+      }
+    })
+  ]
+
+  depends_on = [
+    helm_release.argo-cd,
+    kubernetes_manifest.argocd-github-access,
+    kubernetes_namespace.monitoring,
+  ]
+}
+
 # resource "helm_release" "tempo" {
 #   name = "tempo"
 #   repository = "https://grafana.github.io/helm-charts"
